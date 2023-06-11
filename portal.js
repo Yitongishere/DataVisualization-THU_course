c3b3582e0f008732b3921a3740bcd6939e0ee41d
Yitongishere <jinyt1997@163.com>
2023-06-11 23:25:34 +0800

    portal modi

--- portal.js
+++ portal.js
@@ -30,31 +30,56 @@ function showLogo(svg, teamData, i) {
     .style('font', '13px sans-serif')
     .text(`${processedData.team}`);
 
-    // 全局图标
-    const pano_logo = svg.append("svg:image")
-    .attr('x', width/2 - 60)
-    .attr('y', 320)
-    .attr('width', 120)
-    .attr('height', 120)
-    .attr("xlink:href", `./Logo/Z_panoramic.png`)
-
-    // “全局视角”字样
-    svg.append("text")
-    .attr('x', width/2)
-    .attr('y', 300)
-    .attr("text-anchor", "middle")
-    .text("全局视角");
-
-    
+    mouseon(teams_logo)
+    mouseoff(teams_logo)
     click_logo(teams_logo, processedData.team)
+    console.log(i)
+
+    // 全局图标
+    if (i == 19) {
+        const pano_logo = svg.append("svg:image")
+        .attr('x', width/2 - 60)
+        .attr('y', 320)
+        .attr('width', 120)
+        .attr('height', 120)
+        .attr("xlink:href", `./Logo/Z_panoramic.png`)
+
+        // “全局视角”字样
+        svg.append("text")
+        .attr('x', width/2)
+        .attr('y', 300)
+        .attr("text-anchor", "middle")
+        .text("全局视角");
+
+        mouseon(pano_logo)
+        mouseoff(pano_logo)
+        click_pano(svg, pano_logo)
+    }
+}
+
+function mouseon(target) {
+    target.on('mouseover', () => {
+        target.style("opacity", 0.5);
+    })
+}
+
+function mouseoff(target) {
+    target.on('mouseout', () => {
+        target.style("opacity", 1);
+    })
 }
 
 function click_logo(target, team) {
         target.on('click', () => {
             getAllPic(team)
-})
+})}
 
-    }
+function click_pano(svg, target) {
+    target.on('click', () => {
+        svg.selectAll("*").remove();
+    })
+}
+
 
 
 function calculatePosition(i, size, width) {
@@ -62,8 +87,8 @@ function calculatePosition(i, size, width) {
     per_line = Math.floor(width / l);
     var row = Math.floor(i / per_line); // 计算行数
     var col = Math.floor(i % per_line); // 计算列数
-    var x = col * l; // 计算 x 坐标
-    var y = row * l; // 计算 y 坐标
+    var x = col * l; // 计 x 坐标
+    var y = row * l; // 计算 y算 坐标
     return {
         x: x + size,
         y: y + size
